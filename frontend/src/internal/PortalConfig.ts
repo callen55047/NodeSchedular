59a243efddf0273a694fcb8818b40c246baca571
Callen Egan <callenegan94@gmail.com>
2025-03-13 08:47:18 -0700

    updated config

--- frontend/src/internal/PortalConfig.ts
+++ frontend/src/internal/PortalConfig.ts
@@ -1,6 +1,6 @@
 const PortalConfig = {
   API_LICENSE: 'HsVdNfkFD8il5cyKZwd15VPGPoM3o3xi5oLb9y9ut6IEtcTe',
-  GOOGLE_API_LICENSE: 'AIzaSyC8c9qf9h-MZI0CeNei2udBvvck5wVRTfE'
+  GOOGLE_API_LICENSE: ''
 }
 
 export default PortalConfig
